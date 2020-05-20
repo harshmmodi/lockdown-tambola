fe364398f1548fe9e31f9f636b252f9e4bd41bd0
Harsh Manish Modi <modihar@amazon.com>
2020-05-21 00:56:25 +0530

    Updates

---
+++ precache-manifest.e78c3b18b5168b3ad4ff200e3ecb23de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb8985c1d0caad53633ec383740fa02",
+    "url": "/lockdown-tambola/index.html"
+  },
+  {
+    "revision": "7ff421e7bf1123f0b60e",
+    "url": "/lockdown-tambola/static/css/main.8932ca84.chunk.css"
+  },
+  {
+    "revision": "22c822a27efa5e0fe4fe",
+    "url": "/lockdown-tambola/static/js/2.7ec45492.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lockdown-tambola/static/js/2.7ec45492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff421e7bf1123f0b60e",
+    "url": "/lockdown-tambola/static/js/main.59c3a2ba.chunk.js"
+  },
+  {
+    "revision": "4909e99595e1ce53d74f",
+    "url": "/lockdown-tambola/static/js/runtime-main.d15951ca.js"
+  }
+]);
